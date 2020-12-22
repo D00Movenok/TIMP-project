327edd4c02dcfa6aeb4d7dcb444db9c5c9b5e096
D00Movenok <egor.gennadjev@yandex.ru>
2020-12-22 09:12:03 +0300

    added admin creating on start and rebrend to 31337

--- settings.py
+++ settings.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from hashlib import sha256
 
 
 salt = 'Qwerty1337'
+admin_password = 'admin'
 
 app = Flask(__name__, static_folder='static')
 app.secret_key = salt
@@ -24,3 +26,11 @@ import models
 import routes
 
 db.create_all()
+
+if not models.User.query.filter_by(login='admin').first():
+    password = sha256((admin_password+salt).encode()).hexdigest()
+    new_user = models.User(login='admin',
+                        password=password,
+                        is_admin=True)
+    db.session.add(new_user)
+    db.session.commit()
